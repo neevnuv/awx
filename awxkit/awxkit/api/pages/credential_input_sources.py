7f8316acd091f582de2133b6521153d87b8cdf73
neevnuv <neevnuv@gmail.com>
2024-01-24 09:08:30 +0000

    change from usage of credential_input_source to related_input_source

--- awxkit/awxkit/api/pages/credential_input_sources.py
+++ awxkit/awxkit/api/pages/credential_input_sources.py
@@ -7,11 +7,11 @@ class CredentialInputSource(base.Base):
     pass
 
 
-page.register_page([resources.credential_input_source, (resources.credential_input_sources, 'post')], CredentialInputSource)
+page.register_page(resources.related_input_sources, CredentialInputSource)
 
 
 class CredentialInputSources(page.PageList, CredentialInputSource):
     pass
 
 
-page.register_page([resources.credential_input_sources, resources.related_input_sources], CredentialInputSources)
+page.register_page([resources.credential_input_source, resources.related_input_sources], CredentialInputSources)
